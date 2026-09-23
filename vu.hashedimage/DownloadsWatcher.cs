9fd31342649cd02e8714031664db693e42e1da4e
lusa <hohner2008@gmail.com>
2026-09-23 20:34:21 +0300

    DownloadsWatcher

--- vu.hashedimage/DownloadsWatcher.cs
+++ vu.hashedimage/DownloadsWatcher.cs
@@ -4,6 +4,7 @@ public class DownloadsWatcher
 {
     private FileSystemWatcher _watcher;
     
+    
     public DownloadsWatcher()
     {
         _watcher = new FileSystemWatcher(Utils.GetDownloadFolder());
